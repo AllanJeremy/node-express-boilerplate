2c393f027906fed046af6a20265d3197579321a4
AllanJeremy <aj.dev254@gmail.com>
2021-04-06 22:20:35 +0300

    [Feature - Middleware] Added utility middleware This middleware contains functions that aid in creating our APIs using the ApiUtil

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,6 +2,7 @@
     This file is simply meant to be an aggregator of all middleware related functions.
     This file does not implement any logic of its own and simply serves the purpose of importing functions and exporting them out together so that they can be loaded from one file.
 */
+const UtilityMiddleware = require("./UtilityMiddleware");
 //TODO: Import middleware to aggregate here. Each middleware type usually belongs to its own directory with its own aggregator for its functions eg. PermissionMiddleware/index.js
 
 //* EXPORTS
